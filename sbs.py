111ecda19fb9d471efd70dc34bbb51d77704bc3e
Jake Bradford <jacob.bradford@connect.qut.edu.au>
2018-05-15 14:10:57 +1000

    dev/fixes2
    - added in functionality to record start and end time of child processes
    - system utilisation by child processes is now recorded (aggregated with parent process data)
    - tidied and documented

--- sbs.py
+++ sbs.py
@@ -13,6 +13,9 @@ class Tee(object):
         for f in self.files:
             f.flush()
 
+def listToCsv(list):
+	return ','.join(map(str,list))
+			
 def main(cmd, outFile, sleepTime, loggable):
 
     # check if file exists
@@ -21,7 +24,7 @@ def main(cmd, outFile, sleepTime, loggable):
             print 'Goodbye.'
             quit()        
 
-    with open(outFile, 'w+') as f:
+    with open(outFile, 'w+') as f, open(('%s_children' % (outFile)), 'w+') as fChild:
         # write CSV headers
         f.write('time,num_threads,cpu_percent,mem_rss,mem_vms,io_read_count,io_read_bytes,io_write_count,io_write_bytes,child_process_count\n')
         
@@ -37,38 +40,70 @@ def main(cmd, outFile, sleepTime, loggable):
         print '\nProcess launched: %s' % (p)
         
         # Monitor process.
-        progressIndicator = 0
+        progressIndicator = 0 
+        cpuInterval = 0.1
+        childProcessHistory = [] # all time history
         while p.is_running() and p.status() != psutil.STATUS_ZOMBIE:
+			# start time of this iteration
+			iterTime = time.time()
+			
+			# on the current iteration. cleared every iteration.
+			childProcessHistoryCurrent = [] 
+			
+			# measurement data. start fresh every iteration.
 			measurements = []
-			with p.oneshot():
-				mem = p.memory_full_info()
-				io = p.io_counters()
+			
+			# start monitoring parent process
+			try:
+				with p.oneshot():
+					mem = p.memory_info()
+					io = p.io_counters()
 
-				measurements = [time.time(), # current time
-					p.num_threads(), # number of threads for p
-					p.cpu_percent(interval=0), # >100% indicates multi-core
-					mem.rss, # Resident Set Size (physical mem)
-					mem.vms, # Virtual Memory Size (phy+vir mem),
-					io.read_count,
-					io.read_bytes,
-					io.write_count,
-					io.write_bytes,
-					len(p.children())
-				]
+					measurements = [iterTime,
+						p.num_threads(), # number of threads for p
+						0, # >100% indicates multi-core
+						mem.rss, # Resident Set Size (physical mem)
+						mem.vms, # Virtual Memory Size (phy+vir mem),
+						io.read_count,
+						io.read_bytes,
+						io.write_count,
+						io.write_bytes,
+						len(p.children())
+					]
+					
+				# measure CPU usage outside of the context manager so that we get an accurate reading	
+				measurements[2] = p.cpu_percent(interval=cpuInterval)
 				
+			except Exception as e:
+				print e
+			
+			# start monitoring children too
+			
+			# keep a record of the number of active children so that we can subtract an 
+			# appropriate amount of time from the sleep duration at the end of this iteration
+			activeChildCount = 0 
+			try:
 				for child in p.children(recursive=True):
-					child = psutil.Process(child.pid)
 					if child.is_running() and child.status() != psutil.STATUS_ZOMBIE:
+						activeChildCount = activeChildCount + 1
+						
+						# record any new child processes that have not been seen yet
+						childVector = [child.pid, child.create_time(), '"%s"'%(' '.join(child.cmdline()))]
+						if childVector not in childProcessHistory:
+							childProcessHistory.append(childVector)
+							
+						# keep a record of every child process that has been seen in this iteration	
+						childProcessHistoryCurrent.append(childVector)
+						
+						# get the system utilisation by the child
+						child = psutil.Process(child.pid)
 						with child.oneshot():
 							# We will rought it with some hard code
-							print child.name
-							temp = measurements[2]
-							mem = child.memory_full_info()
+							mem = child.memory_info()
 							io = child.io_counters()
-							print 'sys cpu: %s\n' % psutil.cpu_percent()
-							print 'child cpu: %s\n' % child.cpu_percent(interval=0)
+
 							measurements[1] = measurements[1] + child.num_threads()
-							measurements[2] = measurements[2] + child.cpu_percent(interval=0)
+							
 							measurements[3] = measurements[3] + mem.rss
 							measurements[4] = measurements[4] + mem.vms
 							measurements[5] = measurements[5] + io.read_count
@@ -76,33 +111,41 @@ def main(cmd, outFile, sleepTime, loggable):
 							measurements[7] = measurements[7] + io.write_count
 							measurements[8] = measurements[8] + io.write_bytes
 							measurements[9] = measurements[9] + len(child.children())
-							print 'CPU before: %s\nCPU after: %s' % (temp, measurements[2])
+							
+						# measure CPU usage outside of the context manager so that we get an accurate reading
+						foo = child.cpu_percent(interval=cpuInterval)
+						measurements[2] = measurements[2] + foo
 				
-				f.write('%s\n' % (','.join(map(str, measurements))))
+				# check for any child processes we've seen so far, that did not appear in this iteration
+				# (ie: processes that must have ended). we need the end time for that process.
+				# this is only an estimate as the process may have ended after the start of the 
+				# last iteration, but before now. see example timeline below.
+				# lastIter ------ endTime ------------------- now (now is recorded as the end time)
+				for i in childProcessHistory:
+					if i not in childProcessHistoryCurrent:
+						i.append(iterTime)
 				
-				# f.write('%s\n' % () % (
-					# time.time(), # current time
-					# p.num_threads(), # number of threads for p
-					# p.cpu_percent(interval=0), # >100% indicates multi-core
-					# mem.rss, # Resident Set Size (physical mem)
-					# mem.vms, # Virtual Memory Size (phy+vir mem),
-					# io.read_count,
-					# io.read_bytes,
-					# io.write_count,
-					# io.write_bytes,
-					# len(p.children()) # child processes
-				# ))
+			except Exception as e:
+				print e
+			
+			# monitoring of parent and children complete. write data to file.
+			f.write('%s\n' % (','.join(map(str, measurements))))
 
+			# flush the write buffer if the user desires
 			if loggable == 'y':
 				f.flush()
 
-			# sleep for desired time
+			# show an indicator that the script is alive and sleep for desired time
 			print '.' * ((progressIndicator) % 11)
 			progressIndicator += 1
-			time.sleep(sleepTime)
+			time.sleep(sleepTime - activeChildCount * cpuInterval)
 
+        # write child process history to file
+        fChild.write('\n'.join(map(listToCsv, childProcessHistory)))
         print 'Process exited. Exiting.'
         
+		
+		
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Software Benchmarking Script')
     parser.add_argument('-c', help='Command to run')
