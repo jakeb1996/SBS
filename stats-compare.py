f40a6670be6c2234cfaf115b974a1ea520d2f19d
Jake Bradford <jacob.bradford@connect.qut.edu.au>
2018-07-16 14:40:11 +1000

    dev/stats-expand
    - added new script: stats-children.py
    - now able to analyse time spent in each child process
    - plot a pie chart per test where each wedge is the total time spent in a child process (even if the child process runs twice. ie: notepad was launched twice. what percentage of these two launches contributed to the overall execution time of the test?)
    - plot a bar chart to compare run time of each child process type (ie: shown on a bar graph; notepad ran for X seconds in test A, and Y seconds in test B)
    - upaded stats-compare.py so that the configuration file was more user friendly and compatible with the new stats-children.py script

--- stats-compare.py
+++ stats-compare.py
@@ -135,7 +135,7 @@ class StatsHandlerClass():
                 
                 for line in configFileRaw.split('\n'):
                     lineSplit = line.split(',')
-                    self.tests.append(Test(lineSplit[0], lineSplit[1], lineSplit[2], lineSplit[3], lineSplit[4], lineSplit[5]))
+                    self.tests.append(Test(lineSplit[0], lineSplit[1], lineSplit[2], lineSplit[3], lineSplit[4]))
                     
                     if lineSplit[0] not in self.toolNames:
                         self.toolNames.append(lineSplit[0])
@@ -156,7 +156,7 @@ class StatsHandlerClass():
                 legend = []
                 
                 for test in self.tests:
-                    if test.inputSize == testSize:
+                    if test.inputSize == testSize and test.toolName in ['CRISPR-ERA', 'CasFinder', 'CHOP-CHOP', 'mm10-CRISPR-Database']:
                         if 'y-data-calc' in plots[measurement]:
                             data.append(map(plots[measurement]['y-data-calc'],test.measurements[measurement]))
                         else:
@@ -165,7 +165,7 @@ class StatsHandlerClass():
                         timePoints.append(test.elapsedTimePoints)
                 
                 ### Box plot (x-axis: tool-name, y-axis: system resource measurement)
-                fig = plt.figure(figsize=(16, 6))
+                fig = plt.figure(figsize=(18, 6))
                 fig = fig.add_subplot(111)
                 #plt.yscale('log')
                 fig.boxplot(data, labels=legend, showfliers=False)
@@ -180,7 +180,7 @@ class StatsHandlerClass():
                 plt.clf()
                 
                 ### Scatter plot (x-axis: tool-name, y-axis: system resource measurement)
-                fig = plt.figure(figsize=(16, 6))
+                fig = plt.figure(figsize=(18, 6))
                 fig = fig.add_subplot(111)
                 i = 0
                 while i < len(data):
@@ -208,7 +208,7 @@ class StatsHandlerClass():
                     
             data, legend = self.sortDataAndLabelsDescending(legend, data)
 
-            fig = plt.figure(figsize=(16, 6))
+            fig = plt.figure(figsize=(18, 6))
             fig = fig.add_subplot(111)
             fig.bar(legend, data)
             fig.set_title('%s - %s' % ('Runtime', testSize))
@@ -231,7 +231,7 @@ class StatsHandlerClass():
         for test in self.tests:
             data[VALID_TEST_INPUT_SIZES.index(test.inputSize)].append(test.elapsedTimePoints[-1]) 
         # linear y scale
-        fig = plt.figure(figsize=(16, 6))
+        fig = plt.figure(figsize=(18, 6))
         fig = fig.add_subplot(111)
         fig.boxplot(data, labels=legend, showfliers=False)
         fig.set_title('Run time vs Input size')
@@ -246,7 +246,7 @@ class StatsHandlerClass():
         plt.clf()
         
         # same plot, log y scale
-        fig = plt.figure(figsize=(16, 6))
+        fig = plt.figure(figsize=(18, 6))
         fig = fig.add_subplot(111)
         fig.boxplot(data, labels=legend, sym='+')
         fig.set_title('Run time vs Input size')
@@ -263,11 +263,12 @@ class StatsHandlerClass():
     
     def drawRunTimeScatterOfAllTools(self):
         ### Run time (scatter - x: input size, y: run time)
-        fig = plt.figure(figsize=(16, 6))
+        fig = plt.figure(figsize=(18, 6))
         fig = fig.add_subplot(111)
         fig.set_title('Run Time')
         fig.set_ylabel('Run Time (seconds)')
         fig.set_xlabel('Input Size')
+        #fig.set_xscale("log")
         legend = []
         for tool in self.toolNames:
             x = []
@@ -278,7 +279,7 @@ class StatsHandlerClass():
                     x.append(test.intInputSize)
             
             
-            if True:#tool in ['CT-Finder', 'WU-CRISPR', 'CRISPR-DO', 'CHOP-CHOP']: #len(y) == 4 and tool not in ['GuideScan']:# and tool not in ['sgRNA Scorer 2.0', 'GT-Scan', 'CRISPOR', 'CT-Finder']:
+            if True: #tool in ['CRISPR-ERA', 'CasFinder', 'CHOP-CHOP', 'mm10-CRISPR-Database']: #['CT-Finder', 'CRISPR-DO', 'CHOP-CHOP', 'mm10-CRISPR-Database']: #len(y) == 4 and tool not in ['GuideScan']:# and tool not in ['sgRNA Scorer 2.0', 'GT-Scan', 'CRISPOR', 'CT-Finder']:
                 legend.append(tool)
                 fig.plot(x, y, marker='.', linewidth=SCATTER_LINE_WIDTH)
         fig.legend(legend, loc='center left', bbox_to_anchor=(1, 0.5))   
@@ -293,19 +294,19 @@ class StatsHandlerClass():
         for test in self.tests:
             toolName = '%s_%s' % (test.toolName, test.inputSize)
             outputPrefix = '%s/tool_%s' % (self.outputDir, toolName)
-            os.system("python plotter.py -f %s -t %s -o %s" % (test.rawDataFileURL, toolName, outputPrefix))
+            os.system("python plotter.py -f %s -t %s -o %s" % (test.aggregateFileURL, toolName, outputPrefix))
     
     def drawInterToolPlots(self):
         # [comparing tools (ie: x-axis: tool name)]  
         self.drawSystemResourcesBoxScatterPerTool()
         
         # [comparing input size (ie: x-axis: input size)]
-        self.drawRunTimeBoxLogLinearPerInputSize()
+        #self.drawRunTimeBoxLogLinearPerInputSize()
         
-        self.drawRunTimeScatterOfAllTools()
+        #self.drawRunTimeScatterOfAllTools()
         
         
-        self.drawSbsPlotterForAllTools()
+        #self.drawSbsPlotterForAllTools()
         
         
         
@@ -325,7 +326,7 @@ class StatsHandlerClass():
                     x.append(test.intInputSize)
 
             ### Scatter plot
-            fig = plt.figure(figsize=(16, 6))
+            fig = plt.figure(figsize=(18, 6))
             fig = fig.add_subplot(111)
             fig.plot(x, y, marker='o', linewidth=SCATTER_LINE_WIDTH)
             fig.set_title('%s - %s' % ('Runtime', tool))
@@ -343,23 +344,22 @@ class StatsHandlerClass():
         return (list(t) for t in zip(*sorted(zip(yData, xLabels), reverse=True)))
     
 class Test():
-    def __init__(self, toolName, testNumber, inputSize, sbsPollingInterval, statsFileURL, rawDataFileURL):
+    def __init__(self, toolName, testNumber, inputSize, sbsPollingInterval, sbsBaseFileName):
         self.toolName = toolName
         self.testNumber = testNumber
         self.inputSize = inputSize
         self.intInputSize = self._humanInputSizeToInteger(self.inputSize)
         self.sbsPollingInterval = sbsPollingInterval
-        self.statsFileURL = statsFileURL
-        self.rawDataFileURL = rawDataFileURL
+        self.aggregateFileURL = '%s_%s' % (sbsBaseFileName, 'aggregate')
         
         self.measurements = {}
         self.elapsedTimePoints = []
         self.loadRawDataFile()
         
     def loadRawDataFile(self):
-        if os.path.exists(self.rawDataFileURL):
-            print 'Reading data file: %s' % self.rawDataFileURL
-            with open(self.rawDataFileURL, 'r') as rawDataFile:
+        if os.path.exists(self.aggregateFileURL):
+            print 'Reading data file: %s' % self.aggregateFileURL
+            with open(self.aggregateFileURL, 'r') as rawDataFile:
                 rawDataFile = rawDataFile.read()
                 
                 # setup measurement raw data arrays
@@ -380,7 +380,7 @@ class Test():
                         self.elapsedTimePoints.append(float(lineSplit[0]) - firstTimePoint)
                         
         else:
-            print 'rawDataFile does not exist: %s' % self.rawDataFileURL
+            print 'rawDataFile does not exist: %s' % self.aggregateFileURL
             exit()
 
     def _humanInputSizeToInteger(self, humanInputSize):
@@ -403,7 +403,7 @@ def main(configFile, outputDirectory):
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Aggregate Plotter for the Software Benchmarking Script')
-    parser.add_argument('-c', help='Configuration file (csv formatted: [toolName,testNumber,inputSize,sbsPollingInterval,statsFileURL])', required=True)
+    parser.add_argument('-c', help='Configuration file (csv formatted: [toolName,testNumber,inputSize,sbsPollingInterval,sbsOutputBaseFileName])', required=True)
     parser.add_argument('-m', help='Inter-tool (comparing same size test from different tools), Intra-tool (comparing different size tests from same tool) [inter|intra]')
     parser.add_argument('-o', help='Output directory (default: current)', required=True)
     parser.add_argument('-dpi', help='Output plot DPI (default: %s)' % (PLOT_OUTPUT_DPI))
